59998fefec3c7ff7d3be6527229ae49e565dee2a
Sunka <unrealthingtriescodedev@gmail.com>
2022-05-04 16:48:07 +0200

    updated readability

--- main.cs
+++ main.cs
@@ -6,20 +6,27 @@ while (i < 5)
     Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
     Console.WriteLine("| Welcome to Labree Banking, Please enter your pincode. | ");
     Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    
     //try catch block to prevent aborting the execution when invalid characters are entered
+    
     try
     {
         string pin = (Console.ReadLine());
+        
         //Check if the pin is within 4-6 digit range
+        
         while (pin.Length > 6)
-        {
+        {            
             Console.WriteLine("Digit limit exceeded, please try again. ");
+            
             pin = Console.ReadLine();
         }
         //Checks if the pin is within 4-6 digit range
+        
         while (pin.Length < 4)
         {
             Console.WriteLine("Invalid pin code, please try again.");
+            
             pin = Console.ReadLine();
         }
     }
@@ -29,30 +36,44 @@ while (i < 5)
         Console.WriteLine("Invalid user input, Please try again. Error: " + e.Message);
     }
     Console.Write("Succes! We are logging you in.");
+    
     Thread.Sleep(500);
     Console.Write(".");
     Thread.Sleep(500);
     Console.Write(".");
     Thread.Sleep(500);
+    
     Console.Write(". | Succes!");
+    
     Thread.Sleep(200);
+    
     Console.WriteLine();
+    
     double bal = 10000;
+    
     Console.WriteLine("Press 1. to see balance");
-    Console.WriteLine("Press 2. to withdraw balance");
+    
+    Console.WriteLine("Press 2. to withdraw balance"); 
+    
     Console.WriteLine("Press 3. to deposit");
+    
     Console.WriteLine("Press 4. to change PIN");
+    
     int ch = int.Parse(Console.ReadLine());
+    
     switch (ch)
     {
         case 1:
+            
             //see balance case
+            
             Console.WriteLine("Your balance is " + bal);
             Console.WriteLine();
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine();
             Console.WriteLine("Press 1. to exit.");
             Console.WriteLine("Press 2. to return to the main menu.");
+            
             int c1 = int.Parse(Console.ReadLine());
             switch (c1)
             {
@@ -66,15 +87,20 @@ while (i < 5)
         case 2:
             //withdraw case
             Console.WriteLine("How much do you want to withdraw?");
+            
             double withdrew = Double.Parse(Console.ReadLine());
             double newwithdrew = (bal - withdrew);
             bal = newwithdrew;
+            
             Console.WriteLine("Succes, current balance: " + bal);
             Console.WriteLine();
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine();
+            
             Console.WriteLine("Press 1. to exit.");
+            
             Console.WriteLine("Press 2. to return to the main menu.");
+            
             int c2 = int.Parse(Console.ReadLine());
             switch (c2)
             {
@@ -87,12 +113,18 @@ while (i < 5)
             break;
         case 3:
             //deposit case
+            
             Console.WriteLine("How much do you want to deposit?");
+            
             double dep = Convert.ToDouble(Console.ReadLine());
             double final = (dep + bal);
+            
             Console.WriteLine("Please insert the money in the ATM");
+            
             Thread.Sleep(2000);
+            
             Console.Write("Processing");
+            
             Thread.Sleep(500);
             Console.Write(".");
             Thread.Sleep(500);
@@ -108,12 +140,17 @@ while (i < 5)
             Thread.Sleep(500);
             Console.Write(".");
             Console.WriteLine();
+            
             Console.WriteLine("Succes! Your total balance is " + final);
+            
             Console.WriteLine();
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine();
+            
             Console.WriteLine("Press 1. to exit.");
+            
             Console.WriteLine("Press 2. to return to the main menu.");
+            
             int c3 = int.Parse(Console.ReadLine());
             switch (c3)
             {
@@ -125,27 +162,35 @@ while (i < 5)
             }
             break;
         case 4: //change pin case
+            
             Console.WriteLine("Do you want to change your PIN? [Y] [N]");
+            
             string userInput = Console.ReadLine();
+            
             if (userInput == "Y" || userInput == "y")
             {
                 Console.WriteLine("What do you want your new PIN to be? (4-6 digits)");
+                
                 string newpin = (Console.ReadLine());
+                
                 try
                 {
                     //Check if the pin is within 4-6 digit range
                     while (newpin.Length > 6)
                     {
                         Console.WriteLine("Digit limit exceeded, please try again. ");
+                        
                         newpin = Console.ReadLine();
                     }
                     //Checks if the pin is within 4-6 digit range
                     while (newpin.Length < 4)
                     {
                         Console.WriteLine("Invalid pin code, please try again.");
+                        
                         newpin = Console.ReadLine();
                     }
                     Console.WriteLine("Succes! Your new pin is: " + newpin);
+                    
                     break;
                 }
                 //error handeling
